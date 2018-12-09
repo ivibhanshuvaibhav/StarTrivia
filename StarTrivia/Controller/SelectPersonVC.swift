8848263c0a0df287dd2e8c0bc2dbd07b4db04bc8
Vibhanshu Vaibhav <vibhanshu_chaudhary@yahoo.co.in>
2018-12-09 21:14:16 +0530

    Initial commit

--- StarTrivia/Controller/SelectPersonVC.swift
+++ StarTrivia/Controller/SelectPersonVC.swift
@@ -8,11 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SelectPersonVC: UIViewController {
 
+    @IBOutlet weak var stackBg: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        stackBg.layer.cornerRadius = 10
     }
 
 
